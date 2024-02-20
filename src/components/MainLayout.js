12ab1398af3a3f7cb6aa4f7057f78a2d6dd59514
Mansi168 <mansins168@gmail.com>
2024-02-21 01:43:55 +0530

    sidebar done

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -7,6 +7,12 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
+import { RxDashboard } from "react-icons/rx";
+import { TbMilk } from "react-icons/tb";
+import { GiCow } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import { useNavigate } from 'react-router-dom';
+import { GiCash } from "react-icons/gi";
 const { Header, Sider, Content } = Layout;
 
 
@@ -16,29 +22,54 @@ const MainLayout = () => {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
+  const navigate = useNavigate();
+
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h3 className='text-center fs-5 py-1'>
+            <span className='sm-logo'>MH</span>
+            <span className='lg-logo'>Milkhouse</span>
+          </h3>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['']}
+          onClick={({key})=>{
+            if(key=='signout'){
+
+            }else{
+              navigate(key);
+            }
+          }}
           items={[
+           
             {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
+              key: '',
+              icon: <RxDashboard />,
+              label: 'Dashboard',
             },
             {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              key: 'My Profile',
+              icon: <CgProfile />,
+              label: 'My Profile',
             },
             {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
+              key: 'cattle',
+              icon: <GiCow />,
+              label: 'Cattle',
+            },
+            {
+              key: 'products',
+              icon: <TbMilk />,
+              label: 'Products',
+            },
+            {
+              key: 'revenue',
+              icon: <GiCash />,
+              label: 'Revenue',
             },
           ]}
         />
